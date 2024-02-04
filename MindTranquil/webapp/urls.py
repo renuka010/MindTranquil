17f5eae7c9d88b2162e694f81c2ca74b833876a0
Renuka Bhaskaran <renukabhaskaran1395@gmail.com>
2024-02-05 00:49:58 +0530

    UI updated

--- MindTranquil/webapp/urls.py
+++ MindTranquil/webapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'webapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('breathe/', views.breathe, name='breathe'),
+    path('meditate/', views.meditate, name='meditate'),
+    path('stats/', views.stats, name='stats'),
 ]
--- MindTranquil/webapp/views.py
+++ MindTranquil/webapp/views.py
@@ -7,3 +7,11 @@ def index(request):
 #redirect to breathe
 def breathe(request):
     return render(request, 'webapp/breathe.html')
+
+#redirect to meditate
+def meditate(request):
+    return render(request, 'webapp/meditate.html')
+
+#redirect to stats
+def stats(request):
+    return render(request, 'webapp/stats.html')
